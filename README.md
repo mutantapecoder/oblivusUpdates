170ba55273f5f4838b4c7bd24663aecbc430dd34
mutant ape coder <mutantapecoder@gmail.com>
2024-03-28 16:46:20 +0000

    added user balance notification

--- README.md
+++ README.md
@@ -5,11 +5,19 @@
 ### Steps:
 
 * Create a new Gmail account (used to send emails from)
+
 * Add 2FA to account
+
 * Search for app passwords in google account search bar and create a new app password (used later in code)
+
 * Git clone this repo
+
 * Run `npm i` to install packages.
+
 * Add a .env file and populate it with your Oblivus `API_KEY` and `API_TOKEN` and also Gmail `EMAIL_USER` and `EMAIL_PASSWORD`
+
 * In App.js add the ID's of the machine you want to exclude from this in the `exeptionList` var
+
 *  run `node app` in terminal to run app.
+
 *  the default code will run every 5 minutes. to test use the commented out code under it which will run every 10 seconds.
--- app.js
+++ app.js
@@ -8,10 +8,11 @@ const emailUser = process.env.EMAIL_USER;
 const emailPassword = process.env.EMAIL_PASSWORD;
 
 const listVMsURL = `https://api.oblivus.com/cloud/virtualserver/list/?apiKey=${apiKey}&apiToken=${apiToken}`;
+const userDataURL = `https://api.oblivus.com/account/user/?apiKey=${apiKey}&apiToken=${apiToken}`;
 
 const exceptionList = ['vm16169ddd1d5b0'];
 
-function sendEmailNotification(message) {
+function sendEmailNotification(message, messageSubject) {
   console.log(`Sending email notification: ${message}`);
 
   let transporter = nodemailer.createTransport({
@@ -25,7 +26,7 @@ function sendEmailNotification(message) {
   let mailOptions = {
     from: '"Oblivus Update App" oblivusmachineupdates@gmail.com',
     to: 'oblivusmachineupdates@gmail.com, jasel.chauhan@gmail.com',
-    subject: `Oblivus Machine Update`,
+    subject: messageSubject,
     text: `${message}`,
     html: `${message}`,
   };
@@ -35,7 +36,7 @@ function sendEmailNotification(message) {
       return console.log(error);
     }
     console.log('Message sent: %s', info.messageId);
-    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+    // console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
   });
 }
 
@@ -48,7 +49,7 @@ function getMachineData() {
       machineData.map((machine) => {
         if (exceptionList.includes(machine.ID)) {
           console.log(
-            `machine ${machine.name} is on the exception list and therefore email will not be sent`
+            `machine: ${machine.name} is on the exception list and therefore email will not be sent`
           );
           return;
         } else if (machine.status !== 'Running') {
@@ -57,7 +58,8 @@ function getMachineData() {
           );
 
           sendEmailNotification(
-            `Machine: <strong> ${machine.name} </strong> with an ID of: <strong> ${machine.ID} </strong> has a status of ' <strong> ${machine.status} </strong>'. <br><br> Go to <a href="https://console.oblivus.com/dashboard/oblivuscloud/myinstances/">Oblivus Dashboard</a> and reboot to stop your miner from getting rugged. `
+            `Machine: <strong> ${machine.name} </strong> with an ID of: <strong> ${machine.ID} </strong> has a status of ' <strong> ${machine.status} </strong>'. <br><br> Go to <a href="https://console.oblivus.com/dashboard/oblivuscloud/myinstances/">Oblivus Dashboard</a> and reboot to stop your miner from getting rugged. `,
+            `Oblivus Machine Update`
           );
         }
       });
@@ -67,8 +69,26 @@ function getMachineData() {
     });
 }
 
-//runs code every 5 minutes to check for machine udpates
-setInterval(getMachineData, 5 * 60 * 1000);
+function getUserData() {
+  axios
+    .get(userDataURL)
+    .then((response) => {
+      const userData = response.data.data;
+      console.log(userData.balance);
 
-//runs code every 10 seconds instead of every 5 minutes
+      if (userData.balance < 750) {
+        sendEmailNotification(
+          `Wallet Balance is running low, currently at: <strong> $${userData.balance} </strong>. <br><br> Go to <a href="https://console.oblivus.com/dashboard/billing/addbalance//">Oblivus Top Up Balance</a> to topup balance and prevent machine downtime. `,
+          `User Balance Running Low`
+        );
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+setInterval(getUserData, 2 * 60 * 60 * 1000);
+// setInterval(getUserData, 10000);
+setInterval(getMachineData, 5 * 60 * 1000);
 // setInterval(getMachineData, 10000);
